cb39c97d68c13f20ea2bed353e9b0edbf2f0024a
Roopesh <roopeshbabubh@gmail.com>
2023-11-22 21:23:21 +0530

    home-page-update

--- front-end/manga-app/src/components/Card.js
+++ front-end/manga-app/src/components/Card.js
@@ -6,8 +6,7 @@ function Card() {
         <li className="card-item">
             <a className="card-link" title="Damn Demonic Swords" href="https://www.mangageko.com/manga/damn-demonic-swords/">
                 <div className="card-background-image" style={{ backgroundImage: 'url("https://images.novel-fast.club/avatar/288x412/media/manga_covers/damn-magic-swords.jpg")' }}>
-                    <span className="status">New</span>
-                    <h4 className="novel-title">
+                    <h4 className="card-title">
                         Damn Demonic Swords
                     </h4>
                 </div>
